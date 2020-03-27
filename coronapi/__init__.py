298785e70ee943f0a4fb38850fad495bc035d1fe
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-03-27 01:35:28 -0300

    started project

---
+++ coronapi/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+from coronapi.helpers.get_national_data import get_national_data
+from coronapi.helpers.get_regional_data import get_regional_data
+from . import api
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY="dev")
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    app.register_blueprint(api.bp)
+
+    get_regional_data()
+    get_national_data()
+    sched = BackgroundScheduler(daemon=True)
+    sched.add_job(get_regional_data, "interval", minutes=60)
+    sched.add_job(get_national_data, "interval", minutes=60)
+    sched.start()
+
+    return app
---
+++ coronapi/api.py
@@ -0,0 +1,46 @@
+import requests
+
+from flask import Blueprint, jsonify, json, abort, request
+
+from .constants import (
+    NOT_FOUND_ERROR,
+    REGIONAL_PATH,
+    LATEST_NATIONAL_PATH,
+    HISTORICAL_NATIONAL_PATH,
+    NOVEL_COVID_ENDPOINT,
+)
+
+bp = Blueprint("api", __name__, url_prefix="/api")
+
+
+@bp.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+
+@bp.route(REGIONAL_PATH, methods=["GET"])
+def regions():
+    with open("coronapi/data/regional.json") as json_file:
+        data = json.load(json_file)
+        if "id" in request.args:
+            id = int(request.args["id"])
+            if id not in range(1, 17):
+                return abort(404, description=NOT_FOUND_ERROR,)
+            for val in data:
+                if val["regionInfo"]["_id"] == id:
+                    return json.dumps(val, ensure_ascii=False)
+
+        return json.dumps(data, ensure_ascii=False)
+
+
+@bp.route(LATEST_NATIONAL_PATH, methods=["GET"])
+def national_latest():
+    data = requests.request("GET", NOVEL_COVID_ENDPOINT)
+    return json.loads(data.text)
+
+
+@bp.route(HISTORICAL_NATIONAL_PATH, methods=["GET"])
+def national_historical():
+    with open("coronapi/data/national.json") as json_file:
+        data = json.load(json_file)
+        return json.dumps(data, ensure_ascii=False)
