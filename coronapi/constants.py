5f6f62a1da936d010aae037d337d9e39b399ff5d
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-03-27 01:34:58 -0300

    project constants

---
+++ coronapi/constants.py
@@ -0,0 +1,10 @@
+# Constants of the project
+
+NOT_FOUND_ERROR = "Region not found. Please enter a value between 1 and 16."
+NATIONAL_URL = "https://api.datawrapper.de/v3/charts/4jKuJ/assets/4jKuJ.csv"
+REGIONAL_URL = "https://api.datawrapper.de/v3/charts/4HgES/assets/4HgES.csv"
+HISTORICAL_NATIONAL_PATH = "v1/historical/national"
+LATEST_NATIONAL_PATH = "v1/latest/national"
+REGIONAL_PATH = "v1/latest/regions"
+DATA_FOLDER = "coronapi/data"
+NOVEL_COVID_ENDPOINT = "https://corona.lmao.ninja/countries/chile"
