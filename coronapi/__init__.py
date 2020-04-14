aa867b4c922b55af438424bd4e22af11f17f341a
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-04-14 03:44:25 -0400

    env

--- coronapi/__init__.py
+++ coronapi/__init__.py
@@ -4,12 +4,8 @@ from . import api
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY="dev")
 
-    if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        app.config.from_mapping(test_config)
+    app.config.from_mapping(test_config)
     app.register_blueprint(api.bp)
 
     return app
