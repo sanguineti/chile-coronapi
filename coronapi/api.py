ac71018e0ea5cfeadf384876558a2338282084da
Franco Giovanni Sanguineti <44387191+sanguineti@users.noreply.github.com>
2020-05-17 03:34:02 -0400

    Revert "Habilitación de soporte CORS"

--- coronapi/api.py
+++ coronapi/api.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify, json, abort, request, Response
-from flask_cors import CORS
 
 from .constants import (
     NOT_FOUND_REGION_ERROR,
@@ -22,7 +21,6 @@ from coronapi.helpers.get_data import (
 
 
 bp = Blueprint("api", __name__, url_prefix="/api")
-CORS(bp)
 
 
 @bp.errorhandler(404)
@@ -33,10 +31,7 @@ def resource_not_found(e):
 # Historical endpoints
 @bp.route(V3_HISTORICAL_NATIONAL_PATH, methods=["GET"])
 def v3_historical_national():
-    return Response(
-        json.dumps(get_national_data(), ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+    return Response(json.dumps(get_national_data(), ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 @bp.route(V3_HISTORICAL_REGION_PATH, methods=["GET"])
@@ -46,15 +41,9 @@ def v3_historical_regions():
         id = int(request.args["id"])
         if id not in range(1, 17):
             return abort(404, description=NOT_FOUND_REGION_ERROR)
-        return Response(
-            json.dumps(data[str(id)], ensure_ascii=False),
-            content_type="application/json; charset=utf-8",
-        )
+        return Response(json.dumps(data[str(id)], ensure_ascii=False), content_type="application/json; charset=utf-8")
 
-    return Response(
-        json.dumps(data, ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+    return Response(json.dumps(data, ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 @bp.route(V3_HISTORICAL_COMMUNE_PATH, methods=["GET"])
@@ -63,17 +52,11 @@ def v3_historical_communes():
     if "id" in request.args:
         id = int(request.args["id"])
         try:
-            return Response(
-                json.dumps(data[id], ensure_ascii=False),
-                content_type="application/json; charset=utf-8",
-            )
+            return Response(json.dumps(data[id], ensure_ascii=False), content_type="application/json; charset=utf-8")
         except KeyError:
             return abort(404, description=NOT_FOUND_COMMUNE_ERROR)
 
-    return Response(
-        json.dumps(get_communes_data(), ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+    return Response(json.dumps(get_communes_data(), ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 # latest endpoints
@@ -82,10 +65,7 @@ def v3_national_latest():
     data = get_national_data()
     max_key = max(data.keys())
 
-    return Response(
-        json.dumps(data[max_key], ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+    return Response(json.dumps(data[max_key], ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 @bp.route(V3_LATEST_REGIONAL_PATH, methods=["GET"])
@@ -98,15 +78,9 @@ def v3_regional_latest():
         id = int(request.args["id"])
         if id not in range(1, 17):
             return abort(404, description=NOT_FOUND_REGION_ERROR)
-        return Response(
-            json.dumps(data[str(id)], ensure_ascii=False),
-            content_type="application/json; charset=utf-8",
-        )
+        return Response(json.dumps(data[str(id)], ensure_ascii=False), content_type="application/json; charset=utf-8")
 
-    return Response(
-        json.dumps(data, ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+    return Response(json.dumps(data, ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 @bp.route(V3_LATEST_COMMUNES_PATH, methods=["GET"])
@@ -120,17 +94,11 @@ def v3_communes_latest():
         id = int(request.args["id"])
 
         try:
-            return Response(
-                json.dumps(data[id], ensure_ascii=False),
-                content_type="application/json; charset=utf-8",
-            )
+            return Response(json.dumps(data[id], ensure_ascii=False), content_type="application/json; charset=utf-8")
         except KeyError:
             return abort(404, description=NOT_FOUND_COMMUNE_ERROR)
 
-    return Response(
-        json.dumps(data, ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+    return Response(json.dumps(data, ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 # Models endpoints
@@ -139,14 +107,15 @@ def v3_models_regions():
     data = get_regional_template()
     data_dict = dict()
     for key in data:
-        data_dict.update({key: {"region": data[key]["region"]}})
+        data_dict.update(
+            {
+                key: {"region": data[key]["region"]}
+            }
+        )
         for attr, val in data[key]["regionInfo"].items():
             if attr in ["population", "area", "lat", "long", "hdi"]:
                 data_dict[key][attr] = val
-    return Response(
-        json.dumps(data_dict, ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+    return Response(json.dumps(data_dict, ensure_ascii=False), content_type="application/json; charset=utf-8")
 
 
 @bp.route(V3_COMMUNES, methods=["GET"])
@@ -164,8 +133,5 @@ def v3_models_communes():
         )
         for attr, val in data[key]["communeInfo"].items():
             if attr in ["population", "area", "hdi"]:
-                data_dict[key][attr] = val
-    return Response(
-        json.dumps(data_dict, ensure_ascii=False),
-        content_type="application/json; charset=utf-8",
-    )
+                data_dict[key][attr] = val 
+    return Response(json.dumps(data_dict, ensure_ascii=False), content_type="application/json; charset=utf-8")
