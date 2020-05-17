163d5b380f0b11475a1043f6bb96115485de66be
Franco Sanguineti <franco.sanguineti@ug.uchile.cl>
2020-05-17 03:27:34 -0400

    enabled CORS support

--- coronapi/__init__.py
+++ coronapi/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from . import api
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY="dev")
+    CORS(app)
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
